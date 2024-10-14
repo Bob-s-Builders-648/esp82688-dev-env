917ce19e9b1368c896bffbb0d2ec2c2b7a83611c
Vignesh <realvignesh@gmail.com>
2024-10-14 11:21:25 -0700

    chore: nix files added

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "ESP-32 Firmware";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.process-compose-flake.flakeModule];
+
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {pkgs, ...}: {
+
+        devShells = {
+          default = pkgs.mkShell {
+            name = "dev-shell";
+            buildInputs = with pkgs; [
+              esptool
+            ];
+
+            shellHook = ''
+            '';
+          };
+        };
+
+        process-compose = {
+          localpi = {
+            
+          };
+        };
+      };
+    };
+}
